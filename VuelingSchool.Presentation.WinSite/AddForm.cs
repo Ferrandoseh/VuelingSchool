689b1f0ef5255f33c5187db41c013d98963b0010
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-02-20 20:23:25 +0100

    Minor changes

--- VuelingSchool.Presentation.WinSite/AddForm.cs
+++ VuelingSchool.Presentation.WinSite/AddForm.cs
@@ -15,19 +15,15 @@ namespace VuelingSchool.Presentation.WinSite
     public partial class AddForm : Form
     {
         public event StudentAddedHandler OnStudentAdded;
-        Form1 f1;
         public AddForm(Form1 form1)
         {
             InitializeComponent();
-
-            f1 = form1;
-            OnStudentAdded += f1.AddStudent;
+            OnStudentAdded += form1.AddStudent;
         }
         
         private void btAddEvent_Click(object sender, EventArgs e)
         {
             Student s = new Student(tbId.Text, tbName.Text, tbSurname.Text, tbBirthday.Text);
-
             if (OnStudentAdded != null)
                 OnStudentAdded(s);
         }
